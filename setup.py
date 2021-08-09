fabf70074d861a7f1abb0d2aff3c39c0d4fb6e01
Alexey Karpukhin <7701579+std000@users.noreply.github.com>
2021-08-09 13:24:33 +0300

    [TH2-2117] Renamed project (#12)
    * [TH2-2117] Renamed project

--- setup.py
+++ setup.py
@@ -113,14 +113,10 @@ setup(
     long_description_content_type='text/markdown',
     author='TH2-devs',
     author_email='th2-devs@exactprosystems.com',
-    url='https://github.com/th2-net/th2-grpc-generator-template',
+    url='https://github.com/th2-net/th2-grpc-act-uiframework-web-demo',
     license='Apache License 2.0',
     python_requires='>=3.7',
-    install_requires=[
-        'grpcio-tools==1.33.1',
-        'th2-grpc-common==3.1.2',
-        'th2-grpc-hand==2.7.3'
-    ],
+    install_requires=[],
     packages=packages,
     package_data=package_data,
     cmdclass={
